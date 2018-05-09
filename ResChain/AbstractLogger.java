a9c1b494e78e2cc8d42b50f9a817a9f3543b8e08
liusiyu.taloner <1363431425@qq.com>
2018-05-09 19:13:55 +0800

    责任链模式

---
+++ ResChain/AbstractLogger.java
@@ -0,0 +1,11 @@
+package ResChain;
+
+public class AbstractLogger {
+	public static int INFO = 1;
+	public static int DEBUG = 2;
+	public static int FILE = 3;
+
+	protected int level;
+
+	protected 
+}
